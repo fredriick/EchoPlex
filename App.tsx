b3c169e2d23dc86f1be9c6dde058136fc015d8ab
fredriick <wayne.fred45@gmail.com>
2024-05-20 23:26:27 +0100

    Sets up an IBM Watson NLU instance and analyzes text input for emotions using the emotion feature

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import { MemoryUpload } from './src/MemoryUpload';
-
+// import { MemoryUpload } from './src/MemoryUpload';
+import MemoryUpload from "./src/MemoryUpload"
 export default function App() {
   return <MemoryUpload />;
 }
