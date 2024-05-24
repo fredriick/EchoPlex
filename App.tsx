ea647e0d8e60b95f004e52f4d9cbcd0320a2a052
fredriick <wayne.fred45@gmail.com>
2024-05-24 19:24:32 +0100

    Updated imports for App.tsx

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 // import { MemoryUpload } from './src/MemoryUpload';
 import MemoryUpload from "./src/MemoryUpload"
+import { nlu } from './utils/nlu'
 export default function App() {
   return <MemoryUpload />;
 }
